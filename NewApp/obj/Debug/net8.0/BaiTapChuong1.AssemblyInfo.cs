2da88f787a70e5fc7b5c17542c58daefb355f20a
MyVan <myvan480003@gmail.com>
2024-01-22 12:27:47 +0700

    update

--- NewApp/obj/Debug/net8.0/BaiTapChuong1.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/BaiTapChuong1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapChuong1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c714efd0eefe155d04e3082dfc7b0eee990707")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ecfd306a569aae55216678227feb4e4bbc058c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapChuong1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapChuong1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
